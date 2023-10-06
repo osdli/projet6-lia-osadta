7bf6f3ce9a8f6e0e72dffcbd8197219a92c18f46
osdli <lia.osadta@gmail.com>
2023-10-06 16:27:32 +0200

    Fixed book ID display issue and refined book management logic. - Corrected operator precedence issue when displaying book ID. - Made improvements to book addition and removal from pochliste. - Enhanced display functions for better book data handling.

--- js/functions.js
+++ js/functions.js
@@ -28,6 +28,58 @@ async function searchBooks(title, author) {
   }
 }
  
+// Fonction unifiée pour afficher un livre
+function displayBook(book, container, isFromSession=false) {
+  var bookElement = document.createElement("div");
+  bookElement.classList.add("book-card");
+
+  var thumbnailElement = document.createElement("img");
+  thumbnailElement.src = book.volumeInfo?.imageLinks?.thumbnail || book.thumbnail || "img/unavailable.png";
+  thumbnailElement.classList.add("book-thumbnail");
+
+  var titleElement = document.createElement("h3");
+  titleElement.textContent = book.volumeInfo?.title || book.bookTitle;
+
+  var authorsElement = document.createElement("p");
+  authorsElement.textContent = "Auteur(s): " + (book.volumeInfo?.authors?.join(", ") || book.authors || "Auteur inconnu");
+  authorsElement.classList.add("book-authors");
+
+  var descriptionElement = document.createElement("p");
+  descriptionElement.textContent = book.volumeInfo?.description?.substring(0, 200) || book.description || "Information manquante";
+  descriptionElement.classList.add("book-description");
+
+  var idElement = document.createElement("p");
+  idElement.textContent = "Id : " + (book.id || book.bookId);
+  idElement.classList.add("book-id");
+
+  bookElement.appendChild(thumbnailElement);
+  bookElement.appendChild(titleElement);
+  bookElement.appendChild(idElement);
+  bookElement.appendChild(authorsElement);
+  bookElement.appendChild(descriptionElement);
+
+  if(isFromSession) {
+      var removeButton = document.createElement("button");
+      removeButton.classList.add("delete-button");
+      removeButton.innerHTML = '<i class="fas fa-trash"></i>';
+      removeButton.addEventListener("click", function() {
+          removeBookFromSession(book.bookId || book.id);
+      });
+      bookElement.appendChild(removeButton);
+  } else {
+      var bookmarkElement = document.createElement("button");
+      bookmarkElement.classList.add("bookmark-button");
+      bookmarkElement.innerHTML = '<i class="fas fa-bookmark"></i>';
+      bookmarkElement.addEventListener("click", function() {
+          addBookToPochliste(book);
+      });
+      bookElement.appendChild(bookmarkElement);
+  }
+
+  container.appendChild(bookElement);
+}
+
+
 function displayResults(response) {
   const resultsContainer = document.getElementById("resultsContainer");
   resultsContainer.innerHTML = "";
@@ -37,56 +89,16 @@ function displayResults(response) {
       return;
   }
 
-    const books = response.items;
-    // Récupérer les ID de livres déjà dans la poch'liste pour ne pas les réafficher
-    const sessionData = sessionStorage.getItem("pochliste");
-    const pochlisteBooks = sessionData ? JSON.parse(sessionData) : [];
-    const pochlisteBookIds = pochlisteBooks.map(book => book.bookId);
+  const books = response.items;
+  const sessionData = sessionStorage.getItem("pochliste");
+  const pochlisteBooks = sessionData ? JSON.parse(sessionData) : [];
+  const pochlisteBookIds = pochlisteBooks.map(book => book.bookId);
 
-    books.forEach(function(book) {
-        // Si le livre est déjà dans la poch'liste, ne pas le réafficher dans les résultats de recherche
-        if (!pochlisteBookIds.includes(book.id)) {
-            var bookElement = document.createElement("div");
-            bookElement.classList.add("book-card");
-
-            var thumbnailElement = document.createElement("img");
-            thumbnailElement.src = book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.thumbnail : "img/unavailable.png";
-            thumbnailElement.classList.add("book-thumbnail");
-
-            var titleElement = document.createElement("h3");
-            titleElement.textContent = book.volumeInfo.title;
-            titleElement.classList.add("book-title");
-
-            var authorsElement = document.createElement("p");
-            authorsElement.textContent = "Auteur(s): " + (book.volumeInfo.authors ? book.volumeInfo.authors.join(", ") : "Auteur inconnu");
-            authorsElement.classList.add("book-authors");
-
-            var descriptionElement = document.createElement("p");
-            descriptionElement.textContent = book.volumeInfo.description ? book.volumeInfo.description.substring(0, 200) : "Information manquante";
-            descriptionElement.classList.add("book-description");
-
-            var idElement = document.createElement("p");
-            idElement.textContent = "Id : " + book.id;
-            idElement.classList.add("book-id");
-
-            var bookmarkElement = document.createElement("button");
-            bookmarkElement.classList.add("bookmark-button");
-            bookmarkElement.innerHTML = '<i class="fas fa-bookmark"></i>';
-            bookmarkElement.addEventListener("click", function() {
-                addBookToPochliste(book);
-            });
-
-            bookElement.appendChild(bookmarkElement);
-            bookElement.appendChild(titleElement);
-            bookElement.appendChild(idElement);
-            bookElement.appendChild(authorsElement);
-            bookElement.appendChild(descriptionElement);
-            bookElement.appendChild(thumbnailElement);
-            
-
-            resultsContainer.appendChild(bookElement);
-        }
-    });
+  books.forEach(function(book) {
+      if (!pochlisteBookIds.includes(book.id)) {
+          displayBook(book, resultsContainer);
+      }
+  });
 }
 
 function addBookToPochliste(book) {
@@ -119,7 +131,7 @@ function addBookToPochliste(book) {
 
 }
 
-  // Fonction pour ajouter le livre à la session
+// Fonction pour ajouter le livre à la session
 function addBookToSession(book) {
   var sessionData = sessionStorage.getItem("pochliste");
   var pochliste = sessionData ? JSON.parse(sessionData) : [];
@@ -149,43 +161,7 @@ function displayBooksFromSession() {
   pochlisteContainer.innerHTML = "";
 
   pochliste.forEach(function(book) {
-      var bookElement = document.createElement("div");
-      bookElement.classList.add("book-card");   // Utilisation de "book-card" pour uniformiser le style
-
-      var thumbnailElement = document.createElement("img");
-      thumbnailElement.src = book.thumbnail;
-      thumbnailElement.classList.add("book-thumbnail");
-
-      var titleElement = document.createElement("h4");
-      titleElement.textContent = book.bookTitle;
-
-      var authorsElement = document.createElement("p");
-      authorsElement.textContent = "Auteur(s): " + book.authors;
-      authorsElement.classList.add("book-authors");
-
-      var descriptionElement = document.createElement("p");
-      descriptionElement.textContent = book.description;
-      descriptionElement.classList.add("book-description");
-
-      var idElement = document.createElement("p");
-        idElement.textContent = "Id : " + book.bookId;
-        idElement.classList.add("book-id");
-
-      var removeButton = document.createElement("button");
-      removeButton.classList.add("delete-button");
-      removeButton.innerHTML = '<i class="fas fa-trash"></i>';
-      removeButton.addEventListener("click", function() {
-          removeBookFromSession(book.bookId);
-      });
-
-      bookElement.appendChild(thumbnailElement);
-      bookElement.appendChild(titleElement);
-      bookElement.appendChild(idElement);
-      bookElement.appendChild(authorsElement);
-      bookElement.appendChild(descriptionElement);
-      bookElement.appendChild(removeButton);
-
-      pochlisteContainer.appendChild(bookElement);
+      displayBook(book, pochlisteContainer, true);
   });
 }
 
